3de6ee553d04b3795ee89d19e17b63a841427d58
Tomas Sarapura <tomisarapura@gmail.com>
2024-09-23 22:14:40 -0300

    Agregue la barra de arriba y arme el scrollview

--- app/(tabs)/habitos.tsx
+++ app/(tabs)/habitos.tsx
@@ -1,11 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+
 
 const HabitosScreen = () => {
     return (
         <View>
-            <Text>Hábitos</Text>
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>Hábitos</Text>
+            </View>
+            
+            <ScrollView>
+                <View>
+                    <Text>Contenido</Text>
+                </View>
+            </ScrollView>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: 'gray', //Despues lo definimos mejor viendo alguna palera de colores
+        padding: 30,
+        paddingTop: 50,
+        alignItems: 'center',
+    },
+    headerTitle: {
+        color: 'black',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+});
+
 
 export default HabitosScreen
