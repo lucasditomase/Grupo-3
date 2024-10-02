9aafe1d4e88e0c53175ee5277ab584e1f3941c8d
lucasditomase <150253471+lucasditomase@users.noreply.github.com>
2024-10-02 11:03:37 -0300

    Configure las cabeceras de las vistas, el boton para agregar habitos y comence a definir el almacenamiento persistente

--- app/(tabs)/habitos.tsx
+++ app/(tabs)/habitos.tsx
@@ -1,78 +1,99 @@
 import React from "react";
 import { Text, View, StyleSheet, ScrollView, Image, Pressable } from "react-native";
 import { Link } from 'expo-router';
+import { AsyncStorage } from 'react-native';
 
 const HabitosScreen = () => {
+
+    const onPressButton = () => {
+        storeData('Task', 'Tarea')
+        retrieveData('Task')
+    }
+
+    const storeData = async (key: String, value: any) => {
+        try {
+            await AsyncStorage.setItem(key, value);
+        }
+        catch (error) {
+            // Error saving data
+        }
+    };
+
+    const retrieveData = async (key: String) => {
+        try {
+            const value = await AsyncStorage.getItem(key);
+            if (value !== null) {
+                // We have data!!
+                console.log(value);
+                //alert(value)
+            }
+        }
+        catch (error) {
+            // Error retrieving data
+        }
+    };
+
+
     return (
         <View style={{ flex: 1 }}>
-            <View style={{...styles.header, flexDirection: 'row'}}>
-                <Text style={styles.headerTitle}>Hábitos</Text>
-                <Image style={styles.habitosFilter}  
-                    source={require('../../assets/images/bars-filter.png')} 
-                />
-            </View>
+            <Pressable style={styles.button} onPress={onPressButton}>
+                <Text style={styles.buttonText}>Agregar nuevo habito +</Text>
+            </Pressable>
 
-            <Link href="/" asChild>
-                <Pressable>
-                    <Text style={styles.addButtonText}>+</Text>
-                </Pressable>
-            </Link>
-
-
-            <ScrollView>       
-                <View style={[styles.habitosContainer, {flexDirection: 'row'}]}>
+            <ScrollView>
+                <View style={[styles.habitosContainer, { flexDirection: 'row' }]}>
                     <View style={styles.habitosIconContainer}>
-                        <Image style={styles.habitosIconMedidas}  
-                                source={require('../../assets/images/dumbbell-fitness.png')} 
-                            />
+                        <Image style={styles.habitosIconMedidas}
+                            source={require('../../assets/images/dumbbell-fitness.png')}
+                        />
                     </View>
                     <View style={styles.habitosTextosContainer}>
-                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Gimnasio</Text>
-                        <Text style={{fontSize: 16}}>Frecuencia: Diario</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Gimnasio</Text>
+                        <Text style={{ fontSize: 16 }}>Frecuencia: Diario</Text>
                     </View>
                 </View>
-                <View style={[styles.habitosContainer, {flexDirection: 'row'}]}>
+                <View style={[styles.habitosContainer, { flexDirection: 'row' }]}>
                     <View style={styles.habitosIconContainer}>
-                        <Image style={styles.habitosIconMedidas}  
-                                source={require('../../assets/images/meditation.png')} 
-                            />
+                        <Image style={styles.habitosIconMedidas}
+                            source={require('../../assets/images/meditation.png')}
+                        />
                     </View>
                     <View style={styles.habitosTextosContainer}>
-                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Meditacion</Text>
-                        <Text style={{fontSize: 16}}>Frecuencia: Diario</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Meditacion</Text>
+                        <Text style={{ fontSize: 16 }}>Frecuencia: Diario</Text>
                     </View>
                 </View>
-                <View style={[styles.habitosContainer, {flexDirection: 'row'}]}>
+                <View style={[styles.habitosContainer, { flexDirection: 'row' }]}>
                     <View style={styles.habitosIconContainer}>
-                        <Image style={styles.habitosIconMedidas}  
-                                source={require('../../assets/images/coins.png')} 
-                            />
+                        <Image style={styles.habitosIconMedidas}
+                            source={require('../../assets/images/coins.png')}
+                        />
                     </View>
                     <View style={styles.habitosTextosContainer}>
-                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Pagar servicios</Text>
-                        <Text style={{fontSize: 16}}>Frecuencia: Mensual</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Pagar servicios</Text>
+                        <Text style={{ fontSize: 16 }}>Frecuencia: Mensual</Text>
                     </View>
                 </View>
-                <View style={[styles.habitosContainer, {flexDirection: 'row'}]}>
+                <View style={[styles.habitosContainer, { flexDirection: 'row' }]}>
                     <View style={styles.habitosIconContainer}>
-                        <Image style={styles.habitosIconMedidas}  
-                                source={require('../../assets/images/notebook-alt.png')} 
-                            />
+                        <Image style={styles.habitosIconMedidas}
+                            source={require('../../assets/images/notebook-alt.png')}
+                        />
                     </View>
                     <View style={styles.habitosTextosContainer}>
-                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Revisar mi agenda para la semana</Text>
-                        <Text style={{fontSize: 16}}>Frecuencia: Semanal</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Revisar mi agenda para la semana</Text>
+                        <Text style={{ fontSize: 16 }}>Frecuencia: Semanal</Text>
                     </View>
                 </View>
-                <View style={[styles.habitosContainer, {flexDirection: 'row'}]}>
+                <View style={[styles.habitosContainer, { flexDirection: 'row' }]}>
                     <View style={styles.habitosIconContainer}>
-                        <Image style={styles.habitosIconMedidas}  
-                                source={require('../../assets/images/glass.png')} 
-                            />
+                        <Image style={styles.habitosIconMedidas}
+                            source={require('../../assets/images/glass.png')}
+                        />
                     </View>
                     <View style={styles.habitosTextosContainer}>
-                        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Tomar 2 litros de agua</Text>
-                        <Text style={{fontSize: 16}}>Frecuencia: Diario</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Tomar 2 litros de agua</Text>
+                        <Text style={{ fontSize: 16 }}>Frecuencia: Diario</Text>
                     </View>
                 </View>
             </ScrollView>
@@ -80,10 +101,19 @@ const HabitosScreen = () => {
     )
 }
 
-//Despues lo definimos mejor viendo alguna palera de colores
+//Despues lo definimos mejor viendo alguna paleta de colores
 const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black'
+    },
     header: {
-        backgroundColor: 'gray', 
+        backgroundColor: 'gray',
         padding: 30,
         paddingTop: 50,
         alignItems: 'center',
@@ -101,7 +131,7 @@ const styles = StyleSheet.create({
     },
     habitosIconContainer: {
         flex: 1,
-        backgroundColor: 'lightgray', 
+        backgroundColor: 'lightgray',
         justifyContent: 'center',
         alignItems: 'center',
         height: 98, //Cantidad justa para que se vea el borde
@@ -124,13 +154,13 @@ const styles = StyleSheet.create({
         top: '180%',
         transform: [{ translateY: -15 }],
     },
-addButtonText: {
-    color: 'black',
-    fontSize: 60,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 5,
-},
+    buttonText: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
 });
 
 
