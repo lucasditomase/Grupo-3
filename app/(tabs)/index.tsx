e2f803a96d287305295752780f8945e2b9047e0e
lucasditomase <150253471+lucasditomase@users.noreply.github.com>
2024-10-16 11:50:56 -0300

    Merge Lucas into main
    Mergeo los cambios de hace dos dias a main

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,27 @@
-import { Text, View } from "react-native";
+import ProgressCircle from '../views/circulos';
+import progresoScreenStyles from '../styles/progresoStyles';
+import { StyleSheet, Text, View } from "react-native";
 
 const ProgresoScreen = () => {
     return (
         <View>
-
-        </View>
+            <View style={{
+                marginVertical: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <ProgressCircle size={200} strokeWidth={20} progress={75} color="teal" />
+                <Text style={progresoScreenStyles.label}>75% completado</Text>
+            </View >
+            <View style={{
+                marginVertical: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <ProgressCircle size={200} strokeWidth={20} progress={25} color="teal" />
+                <Text style={progresoScreenStyles.label}>35% completado</Text>
+            </View >
+        </View >
     )
 }
 
