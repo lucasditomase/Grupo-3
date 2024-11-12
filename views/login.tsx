75062cbeccf31567c96935ab5582f6cee4cc235d
Tomas Cortina <70075631+tomascortina@users.noreply.github.com>
2024-11-12 19:20:14 -0300

    fix:

--- views/login.tsx
+++ views/login.tsx
@@ -14,28 +14,24 @@ const LoginScreen: React.FC<LoginScreenProps> = ({
 }) => {
     const [email, setEmail] = useState('');
     const [isLogin, setIsLogin] = useState(false);
-    const [nacimientoDia, setDay] = useState('');
-    const [nacimientoMes, setMonth] = useState('');
-    const [nacimientoAnio, setYear] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
 
-    const handleLogin = () => {
-        console.log('Login with:', email, password);
-        onLoginSuccess();
-        onClose();
+    const handleLogin = async () => {
+        const success = await loginUser(email, password);
+        if (success) {
+            onLoginSuccess();
+            onClose();
+        }
     };
 
-    const handleSignUp = () => {
-        console.log(
-            'Sign up with:',
-            username,
-            nacimientoDia,
-            nacimientoMes,
-            nacimientoAnio,
-            email,
-            password
-        );
+    const handleSignUp = async () => {
+        const success = await registerUser(username, email, password);
+        if (success) {
+            handleLogin();
+            onLoginSuccess();
+            onClose();
+        }
     };
 
     return (
@@ -79,59 +75,6 @@ const LoginScreen: React.FC<LoginScreenProps> = ({
                         onChangeText={setUsername}
                         autoCapitalize="none"
                     />
-                    <Text
-                        style={{
-                            fontSize: 16,
-                            fontWeight: 'bold',
-                            marginBottom: 10,
-                        }}
-                    >
-                        {'Fecha de nacimiento'}
-                    </Text>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <TextInput
-                            style={[
-                                loginScreenStyles.input,
-                                { flex: 1, marginRight: 5 },
-                            ]}
-                            placeholder="Dia"
-                            placeholderTextColor="#aaa"
-                            keyboardType="numeric"
-                            value={nacimientoDia}
-                            onChangeText={setDay}
-                            maxLength={2}
-                        />
-                        <TextInput
-                            style={[
-                                loginScreenStyles.input,
-                                { flex: 1, marginHorizontal: 5 },
-                            ]}
-                            placeholder="Mes"
-                            placeholderTextColor="#aaa"
-                            keyboardType="numeric"
-                            value={nacimientoMes}
-                            onChangeText={setMonth}
-                            maxLength={2}
-                        />
-                        <TextInput
-                            style={[
-                                loginScreenStyles.input,
-                                { flex: 1, marginLeft: 5 },
-                            ]}
-                            placeholder="Anio"
-                            placeholderTextColor="#aaa"
-                            keyboardType="numeric"
-                            value={nacimientoAnio}
-                            onChangeText={setYear}
-                            maxLength={4}
-                        />
-                    </View>
                     <TextInput
                         style={loginScreenStyles.input}
                         placeholder="Correo"
