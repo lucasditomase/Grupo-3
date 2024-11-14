cc33790a23b4dfbf0d6fbfc9f6ae03c16f84bdea
Tomas Cortina <70075631+tomascortina@users.noreply.github.com>
2024-11-14 19:41:30 -0300

    Trying the lucas branch (#31)
    * estructura de datos de los habitos, con arreglos y modales para persistir la informacion. falta poder leer de forma dinamica desde la UI
    
    * lectura de arreglo de forma dinamica en la interfaz. nuevos elementos agregados al arreglo no aparecen en la ui (?)
    
    * termine con la logica de recorrer arreglos de forma dinamica en ui. error anterior corregido
    
    * backup de mi branch antes de hacer pull from main
    
    * El perfil es editable, el modal ahora permite ingresar los datos de un habito (como el nombre y la categoria), el progreso muestras los anillos de completado, se hicieron cambios a la UI
    
    * refactoring masivo y arreglo de bugs
    
    * merge con main
    
    ---------
    
    Co-authored-by: lucasditomase <150253471+lucasditomase@users.noreply.github.com>

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,94 +1,28 @@
-import LoginScreen from '../../views/login';
-import ProgressCircle from '../../components/views/circulos';
-import React, { useState } from 'react';
-import progresoScreenStyles from '../../styles/progresoStyles';
-import themeDark from '../../themes/themeDark';
-import themeLight from '../../themes/themeLight';
-import { Text, View, useColorScheme, Modal } from 'react-native';
-
-const isUserLoggedIn = () => {
-    return false;
-};
+import ProgressCircle from '../views/circulos';
+import progresoScreenStyles from '../styles/progresoStyles';
+import { StyleSheet, Text, View } from "react-native";
 
 const ProgresoScreen = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(isUserLoggedIn());
-    const [isLoginVisible, setIsLoginVisible] = useState(!isLoggedIn);
-    const colorScheme = useColorScheme();
-    const isDarkMode = colorScheme === 'dark';
-
-    const handleLoginSuccess = () => {
-        setIsLoggedIn(true);
-        setIsLoginVisible(false);
-    };
-
     return (
-        <View
-            style={[
-                isDarkMode
-                    ? themeDark.darkBackground
-                    : themeLight.lightBackground,
-            ]}
-        >
-            <Modal
-                visible={isLoginVisible}
-                animationType="slide"
-                transparent={true}
-                onRequestClose={() => setIsLoginVisible(false)}
-            >
-                <View
-                    style={{
-                        flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    }}
-                >
-                    <LoginScreen
-                        onClose={() => setIsLoginVisible(false)}
-                        onLoginSuccess={handleLoginSuccess}
-                    />
-                </View>
-            </Modal>
-            {isLoggedIn && (
-                <>
-                    <View
-                        style={{
-                            marginVertical: 50,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <ProgressCircle
-                            size={200}
-                            strokeWidth={20}
-                            progress={75}
-                            color="teal"
-                        />
-                        <Text style={progresoScreenStyles.label}>
-                            75% completado
-                        </Text>
-                    </View>
-                    <View
-                        style={{
-                            marginVertical: 50,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <ProgressCircle
-                            size={200}
-                            strokeWidth={20}
-                            progress={25}
-                            color="teal"
-                        />
-                        <Text style={progresoScreenStyles.label}>
-                            35% completado
-                        </Text>
-                    </View>
-                </>
-            )}
-        </View>
-    );
-};
+        <View>
+            <View style={{
+                marginVertical: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <ProgressCircle size={200} strokeWidth={20} progress={75} color="teal" />
+                <Text style={progresoScreenStyles.label}>75% completado</Text>
+            </View >
+            <View style={{
+                marginVertical: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <ProgressCircle size={200} strokeWidth={20} progress={25} color="teal" />
+                <Text style={progresoScreenStyles.label}>35% completado</Text>
+            </View >
+        </View >
+    )
+}
 
 export default ProgresoScreen;
